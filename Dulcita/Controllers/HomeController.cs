ec8c20f16ce1aae26937677ae5bbdf87df7b0d40
JoseMaksud <jf9718830@gmail.com>
2024-09-05 22:07:38 -0300

    Tentando fazer outras páginas

--- Dulcita/Controllers/HomeController.cs
+++ Dulcita/Controllers/HomeController.cs
@@ -18,7 +18,12 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult Sobre()
+    {
+        return View();
+    }
+
+    public IActionResult Contato()
     {
         return View();
     }
