523366d9538ea0f1af234a9547f272286a48ef09
JoseMaksud <jf9718830@gmail.com>
2024-09-09 20:31:56 -0300

    Criando o HomeVM e a página de Details

---
+++ Dulcita/ViewModels/HomeVM.cs
@@ -0,0 +1,10 @@
+using Dulcita.Models;
+
+namespace Dulcita.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Categoria> Categorias { get; set; }
+        public List<Produto> Produtos { get; set; }
+    }
+}
