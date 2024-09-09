2f5853d3d2708a8cbccf35f04d98d62c5167167b
JoseMaksud <jf9718830@gmail.com>
2024-09-09 20:21:23 -0300

    Adicionando página de cardápio

--- Dulcita/Controllers/HomeController.cs
+++ Dulcita/Controllers/HomeController.cs
@@ -23,6 +23,10 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Cardapio()
+    {
+        return View();
+    }
     public IActionResult Contato()
     {
         return View();
