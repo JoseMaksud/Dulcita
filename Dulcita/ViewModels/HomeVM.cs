062c4e17ba840ee07089875127f17e2ba0619204
JoseMaksud <jf9718830@gmail.com>
2024-09-19 22:27:58 -0300

    Filtros e implementação de opções

--- Dulcita/ViewModels/HomeVM.cs
+++ Dulcita/ViewModels/HomeVM.cs
@@ -1,10 +1,16 @@
+using System.Collections.Generic;
 using Dulcita.Models;
 
 namespace Dulcita.ViewModels
 {
     public class HomeVM
     {
-        public List<Categoria> Categorias { get; set; }
-        public List<Produto> Produtos { get; set; }
+        public List<Categoria> Categorias { get; set; } 
+        public List<Produto> Produtos { get; set; } 
+        public string CurrentSearchTerm { get; set; } 
+        public int? CurrentCategoriaId { get; set; } 
+        public int CurrentPage { get; set; } 
+        public int TotalPages { get; set; } 
+        public int TotalProdutos { get; set; }
     }
 }
