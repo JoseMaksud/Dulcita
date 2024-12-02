d3a6dbd85ac9d59fc1d6b09c7b6be57f44b02df6
José Francisco <125263537+JoseMaksud@users.noreply.github.com>
2024-12-02 20:20:07 -0300

    Restringindo Acesso.

--- Dulcita/Controllers/CategoriasController.cs
+++ Dulcita/Controllers/CategoriasController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Dulcita.Data;
 using Dulcita.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Dulcita.Controllers
 {
+    [Authorize(Roles = "Administrador")]
     public class CategoriasController : Controller
     {
         private readonly AppDbContext _context;
